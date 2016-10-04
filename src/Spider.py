7cf0bc51a47461a4ff51ce6f69679217ac639d14
Jansen,Sam S.J <sam.jansen@student.fontys.nl>
2016-10-04 16:08:16 +0200

    Use only website text instead of whole html markup

--- src/Spider.py
+++ src/Spider.py
@@ -1,7 +1,9 @@
 import re
+from lxml import etree
 
 import requests
 import threading
+import lxml.html
 
 import CsvHelper
 
@@ -31,9 +33,9 @@ class Spider(threading.Thread):
         """Count for every word that needs to be checked the amount of times it's found in the page content.
         Add this result to the UrlResult as a key and value pair."""
 
-        s = requests.Session()
-        s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
-        content = requests.get(url).text
+        html = requests.get(url).text
+        document = lxml.html.document_fromstring(html)
+        content = "\n".join(etree.XPath("//text()")(document))
 
         for word in self.words:
             count = len(re.findall(re.compile(word, re.IGNORECASE), content))
